8c25a20f5eff7ef40a3f92cfc1ce02217f0e92e9
confusedSerge <serge.kotchourko@gmail.com>
2020-06-10 11:31:26 +0200

    added script for teststat

---
+++ scripts/run_teststat.sh
@@ -0,0 +1,17 @@
+matrix_folder_path=$1
+out_name=$2
+
+gold=testsets/wordsim/gold.tsv
+freq=testsets/wordsim/freqs.tsv
+
+# mkdir -p "test_out/ts_$out_name"
+
+for file in $path_to_files/*
+do
+    echo "started with file $file"
+    output=$(python3.7 evaluation/test_statistik.py $file $gold $freq)
+
+    IFS=$'\t' read -ra my_array <<< $output
+    
+    echo "${my_array[1]};${my_array[3]};${my_array[4]};${my_array[5]}" >> test_out/ts_$out_name.csv
+done
