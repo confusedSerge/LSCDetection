5a16b52b4a3ddc643dd7b52a2e7b9c0482f5edb4
confusedSerge <serge.kotchourko@gmail.com>
2020-06-15 13:11:26 +0200

    changes to algon

--- scripts/run_algoN.sh
+++ scripts/run_algoN.sh
@@ -1,21 +1,22 @@
-path_to_files=$1
+matrix=$1
 threshold_min=$2
 threshold_max=$3
 dim_red_min=$4
 dim_red_max=$5
 
-for matrix in $path_to_files/*
+# for matrix in $path_to_files/*
+# do
+for ((i= $threshold_min; i<=$threshold_max; i++))
 do
-  for ((i= $threshold_min; i<=$threshold_max; i++))
+  for ((j= $dim_red_min; j<=$dim_red_max; j++))
   do
-    for ((j= $dim_red_min; j<=$dim_red_max; j++))
-    do
-      mkdir -p "test_out/algo_n/algo_n_${matrix##*/}"
-      outfile=test_out/algo_n/algo_n_${matrix##*/}/algo_n_${matrix##*/}\_$i\_$j
+    mkdir -p "test_out/algo_n/algo_n_${matrix##*/}"
+    outfile=test_out/algo_n/algo_n_${matrix##*/}/algo_n_${matrix##*/}\_$i\_$j
 
-      echo "started with ${matrix##*/}"
+    echo "started with ${matrix##*/}"
+    # echo "outfile will be"
 
-      python3.7 postprocessing/algo_n.py $matrix $outfile $i $j
-    done
+    python3.7 postprocessing/algo_n.py $matrix $outfile $i $j
   done
 done
+# done
