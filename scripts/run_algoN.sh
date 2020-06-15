9d8f2608ad574c551501d1ed8c51653d5a4675a9
confusedSerge <serge.kotchourko@gmail.com>
2020-06-15 13:15:16 +0200

    muh

--- scripts/run_algoN.sh
+++ scripts/run_algoN.sh
@@ -13,7 +13,7 @@ do
     mkdir -p "test_out/algo_n/algo_n_${matrix##*/}"
     outfile=test_out/algo_n/algo_n_${matrix##*/}/algo_n_${matrix##*/}\_$i\_$j
 
-    echo "started with ${matrix##*/}"
+    echo "started with ${matrix##*/} and values thresh: $i, dim_red: $j"
     # echo "outfile will be"
 
     python3.7 postprocessing/algo_n.py $matrix $outfile $i $j
