e8de90f46aeb86e21bd5fbf1b1f1fad71464acf5
Serge <serge.kotchourko@gmail.com>
2020-06-15 11:42:39 +0200

    change to script algoN

--- scripts/run_algoN.sh
+++ scripts/run_algoN.sh
@@ -1,27 +1,21 @@
-matrix=$1
+path_to_files=$1
 threshold_min=$2
 threshold_max=$3
 dim_red_min=$4
 dim_red_max=$5
 
-targets=testsets/wordsim/targets.tsv
-gold=testsets/wordsim/gold.tsv
-outfileCD=outfileCD.tsv
-
-mkdir -p "test_out/algo_n"
-
-for ((i= $threshold_min; i<=$threshold_max; i++))
+for matrix in $path_to_files/*
 do
-  for ((j= $dim_red_min; j<=$dim_red_max; j++))
+  for ((i= $threshold_min; i<=$threshold_max; i++))
   do
-    outfile=test_out/algo_n/algo_n_$i\_$j
-    python3.7 postprocessing/algo_n.py $matrix $outfile $i $j
+    for ((j= $dim_red_min; j<=$dim_red_max; j++))
+    do
+      mkdir -p "test_out/algo_n_${matrix##*/}"
+      outfile=test_out/algo_n_${matrix##*/}/algo_n_${matrix##*/}\_$i\_$j
 
-    python3.7 measures/cd.py $targets $outfile $outfile $outfileCD
-    output=$(python3.7 evaluation/spr.py $outfileCD $gold $matrix gold 1 0 | grep "gold")
+      echo "started with ${matrix##*/}"
 
-    IFS=$'\t' read -ra my_array <<< $output
-
-  echo "$i;$j;${my_array[2]}" >> test_out/algo_n/algo_n_results.csv
+      python3.7 postprocessing/algo_n.py $matrix $outfile $i $j
+    done
   done
 done
