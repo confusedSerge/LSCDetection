f2af12c2fa7dacab146536aeac30da900bd77251
Serge <serge.kotchourko@gmail.com>
2020-06-30 11:48:11 +0200

    mush

--- scripts/run_test_dirty.sh
+++ scripts/run_test_dirty.sh
@@ -7,7 +7,7 @@ mkdir -p test_out/ppa
 echo " $matrix_path_one" >> test_out/ppa/ppa_results.csv
 for i in {0..25} 
 do
-    output=$(python3.7 evaluation/subtask2.py $matrix_path_one$i $matrix_path_two$i $gold_path | grep "$matrix_path_one$i")
+    output=$(python3.7 evaluation/subtask2.py $matrix_path_one$i $matrix_path_two$i $gold_path | grep "${matrix_path_one##*/}$i")
 
     IFS=$'\t' read -ra my_array <<< $output
 
