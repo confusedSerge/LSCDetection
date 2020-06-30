5c1596b2df84bf82e987529bbb1830588e4336ae
Serge <serge.kotchourko@gmail.com>
2020-06-30 10:09:05 +0200

    changed script

--- scripts/run_ppa.sh
+++ scripts/run_ppa.sh
@@ -3,9 +3,9 @@ threshold_min=$2
 threshold_max=$3
 out_name=$4
 
-targets=testsets/wordsim/targets.tsv
-gold=testsets/wordsim/gold.tsv
-outfileCD=outfileCD.tsv
+# targets=testsets/wordsim/targets.tsv
+# gold=testsets/wordsim/gold.tsv
+# outfileCD=outfileCD.tsv
 
 mkdir -p "test_out/ppa_$out_name"
 
@@ -16,10 +16,10 @@ do
 
   python3.7 postprocessing/ppa.py $matrix $outfile $c
 
-  python3.7 measures/cd.py $targets $outfile $outfile $outfileCD
-  output=$(python3.7 evaluation/spr.py $outfileCD $gold $matrix gold 1 0 | grep "gold")
+  # python3.7 measures/cd.py $targets $outfile $outfile $outfileCD
+  # output=$(python3.7 evaluation/spr.py $outfileCD $gold $matrix gold 1 0 | grep "gold")
 
-  IFS=$'\t' read -ra my_array <<< $output
+  # IFS=$'\t' read -ra my_array <<< $output
 
-  echo "$c;${my_array[2]}" >> test_out/ppa_$out_name/ppa_results.csv
+  # echo "$c;${my_array[2]}" >> test_out/ppa_$out_name/ppa_results.csv
 done
