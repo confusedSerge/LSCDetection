38ef9a64d784950c4fa49c4e817662f9b152524a
confusedSerge <serge.kotchourko@gmail.com>
2020-07-02 11:26:14 +0200

    changed how conc srcip works

--- scripts/make_conc.sh
+++ scripts/make_conc.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 matrix_one=$1
 matrix_two=$2
+outfile=$3
 
-cp $matrix_one $matrix_one\_backup
-cp $matrix_two $matrix_two\_backup
+IFS=$' ' read -ra head_one <<< $(head -n 1 $matrix_one)
+IFS=$' ' read -ra head_two <<< $(head -n 1 $matrix_two)
 
-sed -i 1d $matrix_two
+echo "${head_one[0]} ${head_two[0]} ${head_two[1]}" >> $outfile
 
-awk '{print $1=$1"_ " substr($0, index($0,$2))}' $matrix_two >> $matrix_one
+awk '{if(NR>1)print $1=$1"_ " substr($0, index($0,$2))}' $matrix_one >> $outfile
+sed -n '1!p' $matrix_two >> $outfile
+
