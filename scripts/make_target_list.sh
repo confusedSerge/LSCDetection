3819c1016ea7f4e0f2a323e30f1a754a4e28a0a4
Serge <serge.kotchourko@gmail.com>
2020-07-23 14:59:50 +0200

    script for targetwords

---
+++ scripts/make_target_list.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+matrix=$1
+corp=$2
+
+dir=$(dirname $matrix)
+
+awk '{if(NR>1)print $1=$1}' $matrix >> $dir/tmp
+measures/freq.py $dir/tmp $corp $dir/tmp_trg_freq
+# awk '{if($2>=700)print $1=$1}' $dir/tmp_trg_freq >> $dir/new_targets.tsv
+
+rm -f $dir/tmp
+rm -f $dir/tmp_trg_freq
+
