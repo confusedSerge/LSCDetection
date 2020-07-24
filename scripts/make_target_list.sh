211f31b945217e1925c8f373ee33382f5fd00b0b
Serge <serge.kotchourko@gmail.com>
2020-07-24 12:42:14 +0200

    in disbelieve

--- scripts/make_target_list.sh
+++ scripts/make_target_list.sh
@@ -5,7 +5,7 @@ corp=$2
 dir=$(dirname $matrix)
 
 awk '{if(NR>1)print $1=$1}' $matrix >> $dir/tmp
-python3.8 measures/freq_new.py $dir/tmp $corp $dir/tmp_trg_freq
+python3.7 measures/freq.py $dir/tmp $corp $dir/tmp_trg_freq
 # awk '{if($2>=700)print $1=$1}' $dir/tmp_trg_freq >> $dir/new_targets.tsv
 
 rm -f $dir/tmp
