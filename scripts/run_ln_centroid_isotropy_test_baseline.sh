4c970a674af0ccecadcaa01450a396a7495605c7
confusedSerge <serge.kotchourko@gmail.com>
2020-09-07 19:48:36 +0200

    fix

--- scripts/run_ln_centroid_isotropy_test_baseline.sh
+++ scripts/run_ln_centroid_isotropy_test_baseline.sh
@@ -17,10 +17,11 @@ do
         echo "$j"
         out_name=$(basename $j)
 
+        output=$(python3.8 evaluation/test_statistik_wordsim_lengthseparated.py $j $wordsim $freq)
+        
         for ((c=0; c<=25; c++))
         do
             # baseline calculation
-            output=$(python3.8 evaluation/test_statistik_wordsim.py $j $wordsim $freq)
             echo "$c;$output" >> $out_path/results_baseline_$out_name.csv
         done
     done
